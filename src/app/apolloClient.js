09ff00cb43b589d62de747de77d9b01e3dcac273
jancyvarghese90 <jancyvarghese90@gmail.com>
2025-05-21 15:20:45 +0300

    added

--- src/app/apolloClient.js
+++ src/app/apolloClient.js
@@ -2,7 +2,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/', // Replace with your actual backend URL
+  uri: 'https://blog-internship.onrender.com/graphql', // Replace with your actual backend URL
   cache: new InMemoryCache(),
 });
 
